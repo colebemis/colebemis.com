8d48f1d85664368e465d11e02d82e75ce3f2b43d
Cole Bemis <colebemis@gmail.com>
2018-09-30 15:49:53 -0700

    Add favicon

--- build.js
+++ build.js
@@ -7,7 +7,8 @@ const DEST = 'build';
 
 const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
 const markdown = fs.readFileSync(path.resolve(__dirname, 'README.md'), 'utf8');
-const css = fs.readFileSync(path.resolve(__dirname, 'style.css'), 'utf8');
+const css = fs.readFileSync(path.resolve(__dirname, 'style.css'));
+const favicon = fs.readFileSync(path.resolve(__dirname, 'favicon.ico'));
 
 fs.writeFileSync(
   path.resolve(__dirname, DEST, 'index.html'),
@@ -15,6 +16,8 @@ fs.writeFileSync(
 );
 
 fs.writeFileSync(path.resolve(__dirname, DEST, 'style.css'), css);
+
+fs.writeFileSync(path.resolve(__dirname, DEST, 'favicon.ico'), favicon);
 
 function insertMarkdown(html, markdown) {
   const $ = cheerio.load(html);
