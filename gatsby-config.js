4b8412a8fbc6f5f115b5adae885576bbe124e57c
Cole Bemis <colebemis@github.com>
2020-04-01 22:35:59 -0700

    Clean up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: 'Cole Bemis',
-    description:
-      'Frontend engineer, designer, lifelong learner, dog lover. Studying computer science at Cal Poly. Intern on the GitHub Design Systems team.',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-theme-ui',
-    {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        defaultLayouts: {
-          default: require.resolve('./src/components/layout.js'),
-        },
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-theme-ui', 'gatsby-plugin-mdx'],
 }
--- src/components/container.js
+++
@@ -1,8 +0,0 @@
-/** @jsx jsx */
-import { jsx } from 'theme-ui'
-
-function Container(props) {
-  return <div sx={{ maxWidth: '34em', mx: 'auto', px: 5, py: 8 }} {...props} />
-}
-
-export default Container
